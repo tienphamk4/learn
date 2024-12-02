0bc73908aab4d3866f9db43a1642200d720f8c24
phamtienk4 <25a4041924@hvnh.edu.vn>
2024-12-02 14:29:57 +0700

    update news and detail

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,14 +1,3 @@
-document.addEventListener('scroll', function () {
-    var header = this.documentElement.querySelector('.header');
-    const scrollY = window.scrollY;
-
-    if (scrollY <= 150) {
-        header.classList.remove('scrolled')
-    } else {
-        header.classList.add('scrolled')
-
-    }
-})
 
 var menu = document.querySelector('.header_menu')
 
