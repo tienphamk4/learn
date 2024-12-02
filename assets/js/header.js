47749b6fa4c6feaeaafe1d23058d34e65abbebf9
phamtienk4 <25a4041924@hvnh.edu.vn>
2024-12-02 22:05:57 +0700

    add cart page

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,8 +1,16 @@
 
 var menu = document.querySelector('.header_menu')
 
-menu.addEventListener('click', function () {
+menu.addEventListener('click', function (e) {
+    e.stopPropagation();
     var dropdown = document.querySelector('.dropdown')
     dropdown.classList.toggle('hide');
-    console.log(dropdown)
+})
+document.addEventListener('click', function (e) {
+
+    var dropdown = document.querySelector('.dropdown')
+    if (!dropdown.classList.contains('hide')) {
+        dropdown.classList.add('hide')
+        console.log('ok')
+    }
 })
